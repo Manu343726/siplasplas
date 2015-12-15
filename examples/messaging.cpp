bdf6d34051e440ec9b969ea88c00e4c5ac9bb134
Manu343726 <Manu343726@gmail.com>
2015-12-15 08:36:54 +0100

    Messaging example with default handlers

--- examples/messaging.cpp
+++ examples/messaging.cpp
@@ -20,7 +20,7 @@ public:
     void process(Visitor&& visitor) const
     {
         // TODO: Multivisitor here over all args
-        _args[0].template visit<void>(visitor);
+        _args[0].template visit<void>(std::forward<Visitor>(visitor));
     }
 
 private:
@@ -30,26 +30,58 @@ private:
 template<std::size_t Arity, typename T>
 struct MessageHandler
 {
+    template<typename...> class list {};
+    template<typename...> using void_t = void;
+
+    // processor class is a dispatcher to the T::process() function.
+    // It just asks "Has T a process() overload to handle this event?"
+    // If the answer is "yes", calls it, else does nothing.
+    template<typename U, typename Args, typename = void>
+    struct processor;
+
+    template<typename U, typename... Args>
+    struct processor<U, list<Args...>, void_t<decltype(std::declval<U>().process(std::declval<std::decay_t<Args>>()...))>>
+    {
+        static auto apply(U* This, Args&&... args)
+        {
+            This->process(std::forward<Args>(args)...);          
+        }
+    };
+
+    template<typename U, typename... Args, typename _void>
+    struct processor<U, list<Args...>, _void>
+    {
+        // Note a call to an empty function means no call at runtime
+        static void apply(U* This, Args&&... args)
+        {
+            // nop
+        }
+    };
+
+    template<typename... Args>
+    void apply_processor(Args&&... args) const
+    {
+        processor<const T, list<Args...>>::apply(static_cast<const T*>(this),
+                                           std::forward<Args>(args)...);
+    }
+
     void receive(const Message<Arity>& message) const
     {
-        message.process([this](const auto&... args)
-        {
-            // Calls process() of your class (T)
-            static_cast<const T*>(this)->process(args...);
-        });
+        message.process(
+            [this](auto&&... args)
+            {
+                this->apply_processor(std::forward<decltype(args)>(args)...);
+            }
+        ); 
     }
 };
 
+// Google search "CRTP C++" ;)
 struct MyClass : public MessageHandler<1, MyClass>
 {
     void process(const std::string& str) const
     {
         std::cout << "Received a string message!" << std::endl;
-    }
-
-    void process(float number) const
-    {
-        std::cout << "Received a float message!" << std::endl;
     }
 
     void process(const TransformMatrix& matrix) const
