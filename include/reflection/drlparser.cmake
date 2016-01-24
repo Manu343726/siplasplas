a676c5beadd9f562a0c2c30cd63ba855161560a3
Manu343726 <Manu343726@gmail.com>
2016-01-24 03:53:45 +0100

    Reflection toolchain working on Visual Studio

--- include/reflection/drlparser.cmake
+++ include/reflection/drlparser.cmake
@@ -1,4 +1,4 @@
-
+include(cmake/custom_properties)
 
 find_package(PythonInterp 2.7 REQUIRED)
 
@@ -10,26 +10,6 @@ endif()
 
 set(DRLPARSER_SCRIPT include/reflection/DRLParser)
 
-function(generate_reflection_code SOURCEFILE)
-    get_filename_component(PATH ${SOURCEFILE} ABSOLUTE)
-
-    message(STATUS "[Reflection] Processing file '${SOURCEFILE}'...")
-
-    execute_process(COMMAND ${DRLPARSER_SCRIPT} ${PATH}
-        RESULT_VARIABLE result
-        OUTPUT_VARIABLE output
-        ERROR_VARIABLE error)
-
-    message(STATUS "[Reflection] result: ${result}")
-
-    if(NOT (result EQUAL 0))
-        message(STATUS "[Reflection] Error processing ${SOURCEFILE}:")
-        message(STATUS " error code: ${result}")
-        message(STATUS " stderr: ${error}")
-        message(FATAL_ERROR "[Reflection] ABORTED")
-    endif()
-endfunction()
-
 function(reflection_target TARGET)
     if(NOT (TARGET ${TARGET}))
         message(FATA_ERROR "Target ${TARGET} not found!")
@@ -38,32 +18,44 @@ function(reflection_target TARGET)
     get_target_property(SOURCES ${TARGET} SOURCES)
     get_target_property(INCLUDE_DIRS ${TARGET} INCLUDE_DIRECTORIES)
     
-    message(STATUS "[REFLECTION] Processing target ${TARGET}: ${SOURCES}")
+    message(STATUS "[REFLECTION] Processing target ${TARGET}:")
 
     foreach(source ${SOURCES})
-        message(STATUS "[REFLECTION] -> Overriding ${source}...")
+        if(MSVC)
+            source_file_has_custom_property(is_sourcegroups_header "${source}" VS_SOURCEGROUPS_HEADER)
+
+            if(is_sourcegroups_header)
+                continue()
+            endif()
+        endif()
+
+        message(STATUS "[REFLECTION]  - ${source}")
 
         get_filename_component(ext ${source} EXT)
         get_filename_component(name ${source} NAME_WE)
         get_filename_component(path ${source} PATH)
         set(final_source "${path}/${name}.rfl${ext}")
-        
+
+        if(NOT (EXISTS ${final_source}))
+            # Create a fake source file so target SOURCES property check doesn't fail
+            execute_process(COMMAND ${CMAKE_COMMAND} -E touch ${final_source})
+        endif()
+
         list(APPEND final_sources ${final_source})
+        list(APPEND sources ${source})
     endforeach()
 
-    message(STATUS "Final sources: ${final_sources}")
     set_target_properties(${TARGET} PROPERTIES SOURCES ${final_sources})
 
+    message(STATUS "[REFLECTION] Setting preprocessor hook for target ${TARGET}")
     add_custom_target(${TARGET}_prebuild)
     add_dependencies(${TARGET} ${TARGET}_prebuild)
-    
-    message(STATUS "Setting custom pre-build command for target ${TARGET}")
     string(REGEX REPLACE ";" " " SOURCES "${SOURCES}")
     string(REGEX REPLACE ";" " " INCLUDE_DIRS "${INCLUDE_DIRS}")
 
     add_custom_command(
         TARGET ${TARGET}_prebuild POST_BUILD 
-        COMMAND ${CMAKE_SOURCE_DIR}/${DRLPARSER_SCRIPT} -I ${INCLUDE_DIRS} 
-            -f ${SOURCES} -s ${CMAKE_SOURCE_DIR} -x *3rdParty*
+        COMMAND ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/${DRLPARSER_SCRIPT} -I ${INCLUDE_DIRS} 
+            -f ${sources} -s ${CMAKE_SOURCE_DIR} -x *3rdParty*
     )    
 endfunction()
