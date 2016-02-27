61ba9287a3d2ba53d34a22bfa647af5d684cd4ac
Manu343726 <Manu343726@gmail.com>
2016-02-27 17:37:19 +0100

    embedded allocator translation to library

--- include/siplasplas/allocator/embedded_allocator.hpp
+++ include/siplasplas/allocator/embedded_allocator.hpp
@@ -10,72 +10,26 @@ namespace cpp
     class EmbeddedAllocator
     {
     public:
-        EmbeddedAllocator(void* begin, void* end, std::size_t metadata_length = 0) :
-            _begin{reinterpret_cast<char*>(begin)}
-        {
-            detail::write_at(_begin, end);
-            detail::write_at(_begin + sizeof(void*), metadata_length);
-        }
+        EmbeddedAllocator(void* begin, void* end, std::size_t metadata_length = 0);
 
-        char* end() const
-        {
-            return detail::read_at<char*>(_begin);
-        }
+        char* begin() const;
+        char* end() const;
+        char* metadata_begin() const;
+        char* metadata_end() const;
 
-        std::size_t storage_size() const
-        {
-            return end() - begin();
-        }
-
-        std::size_t metadata_length() const
-        {
-            return detail::read_at<std::size_t>(_begin + sizeof(void*));
-        }
-
-        char* metadata_begin() const
-        {
-            return _begin + sizeof(void*) + sizeof(std::size_t);
-        }
-
-        char* metadata_end() const
-        {
-            return metadata_begin() + metadata_length();
-        }
+        std::size_t storage_size() const;
+        std::size_t metadata_length() const;
 
         template<typename T>
-        detail::RawReaderWriter<T> metadata(std::size_t begin)
-        {
-            return { metadata_begin() + begin };
-        }
+        detail::RawReaderWriter<T> metadata(std::size_t begin);
 
         template<typename T>
-        T metadata(std::size_t begin) const
-        {
-            return detail::read_at<T>(metadata_begin() + begin);
-        }
+        T metadata(std::size_t begin) const;
 
-        char* begin() const
-        {
-            return metadata_end();
-        }
+        bool belongs_to_storage(void* pointer) const;
 
-        bool belongs_to_storage(void* pointer) const
-        {
-            return begin() <= pointer && pointer < end();
-        }
+        std::string dump() const;
 
-        std::string dump() const
-        {
-            std::ostringstream os;
-
-            os << "embedded allocator dump:" << std::endl;
-            os << "=========================" << std::endl;
-            os << " - Storage begin: " << (void*)begin() << std::endl;
-            os << " - Storage set end: " << (void*)end() << std::endl;
-            os << " - Metadata length: " << metadata_length() << std::endl;
-
-            return os.str();
-        }
     private:
         char* _begin;
     };
---
+++ src/allocator/embedded_allocator.cpp
@@ -0,0 +1,71 @@
+#include "siplasplas/allocator/embedded_allocator.hpp"
+
+using namespace cpp;
+
+EmbeddedAllocator::EmbeddedAllocator(void* begin, void* end, std::size_t metadata_length) :
+    _begin{reinterpret_cast<char*>(begin)}
+{
+    detail::write_at(_begin, end);
+    detail::write_at(_begin + sizeof(void*), metadata_length);
+}
+
+char* EmbeddedAllocator::end() const
+{
+    return detail::read_at<char*>(_begin);
+}
+
+std::size_t EmbeddedAllocator::storage_size() const
+{
+    return end() - begin();
+}
+
+std::size_t EmbeddedAllocator::metadata_length() const
+{
+    return detail::read_at<std::size_t>(_begin + sizeof(void*));
+}
+
+char* EmbeddedAllocator::metadata_begin() const
+{
+    return _begin + sizeof(void*) + sizeof(std::size_t);
+}
+
+char* EmbeddedAllocator::metadata_end() const
+{
+    return metadata_begin() + metadata_length();
+}
+
+template<typename T>
+detail::RawReaderWriter<T> EmbeddedAllocator::metadata(std::size_t begin)
+{
+    return { metadata_begin() + begin };
+}
+
+template<typename T>
+T EmbeddedAllocator::metadata(std::size_t begin) const
+{
+    return detail::read_at<T>(metadata_begin() + begin);
+}
+
+char* EmbeddedAllocator::begin() const
+{
+    return metadata_end();
+}
+
+bool EmbeddedAllocator::belongs_to_storage(void* pointer) const
+{
+    return begin() <= pointer && pointer < end();
+}
+
+std::string EmbeddedAllocator::dump() const
+{
+    std::ostringstream os;
+
+    os << "embedded allocator dump:" << std::endl;
+    os << "=========================" << std::endl;
+    os << " - Storage begin: " << (void*)begin() << std::endl;
+    os << " - Storage set end: " << (void*)end() << std::endl;
+    os << " - Metadata length: " << metadata_length() << std::endl;
+
+    return os.str();
+}
+
