c626b6a509f3ef0ba6854221182b4bedcbe15521
Manu343726 <Manu343726@gmail.com>
2016-03-24 21:45:37 +0100

    [reflection engine rework] metaobject menipulation utilities

---
+++ include/reflection/detail/metaobject_manip.hpp
@@ -0,0 +1,79 @@
+#ifndef SIPLASPLAS_REFLECTION_DETAIL_METAOBJECT_MANIP_HPP
+#define SIPLASPLAS_REFLECTION_DETAIL_METAOBJECT_MANIP_HPP
+
+#include "metaobject.hpp"
+#include <siplasplas/utility/meta.hpp>
+#include <siplasplas/utility/function_traits.hpp>
+#include <vector>
+
+namespace cpp
+{
+
+template<typename... Args>
+std::vector<cpp::MetaObject> pack_to_vector(Args&&... args)
+{
+    return {cpp::MetaObject{args}...};
+}
+
+namespace
+{
+    template<typename... Args, std::size_t... Is>
+    std::tuple<Args...> vector_to_tuple(const std::vector<cpp::MetaObject>& vector, meta::index_sequence<Is...>)
+    {
+        return {vector[Is].get<Args>()...};
+    }
+
+    template<typename... Args, std::size_t... Is>
+    std::vector<cpp::MetaObject> tuple_to_vector(const std::tuple<Args...>& tuple, meta::index_sequence<Is...>)
+    {
+        return {cpp::MetaObject{std::get<Is>(tuple)}...};
+    }
+
+    template<typename Function, std::size_t... Is>
+    auto vector_call(Function function, const std::vector<cpp::MetaObject>& args, meta::index_sequence<Is...>)
+    {
+        return function(args[Is].get<cpp::function_argument<Is, Function>>()...);
+    }
+}
+
+template<typename... Args>
+std::tuple<Args...> vector_to_tuple(const std::vector<cpp::MetaObject>& vector)
+{
+    return vector_to_tuple<Args...>(vector, meta::make_index_sequence_for<Args...>{});
+}
+
+template<typename... Args>
+std::vector<cpp::MetaObject> tuple_to_vector(const std::tuple<Args...>& tuple)
+{
+    return tuple_to_vector(tuple, meta::make_index_sequence_for<Args...>{});
+}
+
+template<typename Function>
+auto vector_call(Function function, const std::vector<cpp::MetaObject>& vector)
+{
+    return vector_call(function, vector, meta::to_index_sequence_t<typename cpp::function_signature<Function>::args>{});
+}
+
+template<typename Class, typename R, typename... Args>
+R vector_call(R (Class::* function)(Args...) const, const Class& object, const std::vector<cpp::MetaObject>& vector)
+{
+    return vector_call([function,&object](Args... args)
+    {
+        return (object.*function)(args...);
+    },
+    vector);
+}
+
+template<typename Class, typename R, typename... Args>
+R vector_call(R (Class::* function)(Args...), Class& object, const std::vector<cpp::MetaObject>& vector)
+{
+    return vector_call([function,&object](Args... args)
+    {
+        return (object.*function)(args...);
+    },
+    vector);
+}
+
+}
+
+#endif // SIPLASPLAS_REFLECTION_DETAIL_METAOBJECT_MANIP_HPP
