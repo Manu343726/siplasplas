33d2fa0a4454bab536a4e6d23275415fa979214b
Manu343726 <Manu343726@gmail.com>
2016-03-25 19:21:58 +0100

    FunctionFamily class __init__ function. It handled incoming method cursors and rearranges then in the parent as families of Method nodes (Families are identified by the cursor spelling, each family entry by the cursor displayname)

--- include/reflection/parser/ast/functions.py
+++ include/reflection/parser/ast/functions.py
@@ -4,3 +4,27 @@ class Method(Node):
 
     def __init__(self, **kwargs):
         Node.__init__(self, **kwargs)
+
+
+class OverloadedMethod(Method):
+
+    def __init__(self, **kwargs):
+        Method.__init__(self, **kwargs)
+
+
+class MethodFamily(Node):
+
+    def __init__(self, **kwargs):
+        cursor = kwargs['cursor']
+        parent = kwargs['parent']
+
+        if cursor.spelling in parent.children['methodfamily']:
+            family = parent.children['methodfamily'][cursor.spelling]
+            family[cursor.displayname] = OverloadedMethod.create_node(cursor, parent)
+        else:
+            if cursor.spelling in parent.children['method']:
+                parent.children['methodfamily'][cursor.spelling] = {}
+                parent.children['methodfamily'][cursor.spelling][cursor.displayname] = OverloadedMethod.create_node(cursor, parent)
+            else:
+                if parent.children['methodfamily']
+                    parent.children['method'][cursor.spelling] = Method.create_node(cursor, parent)
