9bd027d2dd07d3d7f97b2504be531658b118eb5b
Manu343726 <Manu343726@gmail.com>
2016-05-23 02:18:57 +0200

    Split Object into header + source

--- include/siplasplas/reflection/dynamic/object.hpp
+++ include/siplasplas/reflection/dynamic/object.hpp
@@ -15,89 +15,23 @@ namespace dynamic_reflection
 class Object
 {
 public:
-    template<typename T>
-    static void registerObject()
-    {
-        cpp::dynamic_reflection::Type::registerType<T>();
-    }
-
-    struct Empty {};
-
     static constexpr bool ConstructReference = true;
 
-    Object() :
-        Object{Type::get<Empty>()}
-    {}
-
-    Object(const cpp::dynamic_reflection::Type& type) :
-        _type{type},
-        _object{_type.construct()},
-        _isReference{false}
-    {}
-
-    Object(const cpp::dynamic_reflection::Type& type, void* fromRaw, bool isReference = false) :
-        _type{type},
-        _object{isReference ? fromRaw : _type.copy_construct(fromRaw)},
-        _isReference{isReference}
-    {}
-
+    /// 
+    Object();
+    Object(const cpp::dynamic_reflection::Type& type);
+    Object(const cpp::dynamic_reflection::Type& type, void* fromRaw, bool isReference = false);
     template<typename T>
     Object(const T& value) :
         _type{cpp::dynamic_reflection::Type::get<T>()},
         _object{cpp::dynamic_reflection::Type::get<T>().copy_construct(&value)},
         _isReference{false}
     {}
-
-    Object(const Object& other) :
-        _type{other._type},
-        _object{other._isReference ? other._object : _type.copy_construct(other._object)},
-        _isReference{other._isReference}
-    {}
-
-    Object(Object&& other) :
-        _type{other._type},
-        _object{other._isReference ? other._object : _type.move_construct(other._object)},
-        _isReference{other._isReference}
-    {}
-
-    Object& operator=(const Object& other)
-    {
-        if(_type != other._type)
-        {
-            destroy();
-            _type = other._type;
-            _object = other._isReference ? other._object : _type.copy_construct(other._object);
-            _isReference = other._isReference;
-        }
-        else
-        {
-            _type.copy_assign(_object, other._object);
-        }
-
-        return *this;
-    }
-
-    Object& operator=(Object&& other)
-    {
-        if(_type != other._type)
-        {
-            destroy();
-            _type = other._type;
-            _object = other._isReference ? other._object : _type.move_construct(other._object);
-            _isReference = other._isReference;
-        }
-        else
-        {
-            _type.move_assign(_object, other._object);
-        }
-
-        return *this;
-    }
-
-    ~Object()
-    {
-        destroy();
-    }
+    Object(const Object& other);
+    Object(Object&& other);
+    Object& operator=(const Object& other);
+    Object& operator=(Object&& other);
+    ~Object();
 
     template<typename T>
     Object& operator=(const T& value)
@@ -132,49 +66,22 @@ public:
         return get<T>();
     }
 
-    cpp::dynamic_reflection::Type type() const
-    {
-        return _type;
-    }
+    cpp::dynamic_reflection::Type type() const;
+    bool isReference() const;
+    bool empty() const;
 
-    bool isReference() const
-    {
-        return _isReference;
-    }
+    const void* raw() const;
+    void* raw();
 
-    const void* raw() const
-    {
-        return _object;
-    }
-
-    void* raw()
-    {
-        return _object;
-    }
-
-    std::string toString() const
-    {
-        return _type.toString(_object);
-    }
-
-    static Object fromString(const std::string& typeName, const std::string& value)
-    {
-        auto type = cpp::dynamic_reflection::Type::get(typeName);
-        return Object{type, type.fromString(value)};
-    }
+    std::string toString() const;
+    static Object fromString(const std::string& typeName, const std::string& value);
 
 private:
     cpp::dynamic_reflection::Type _type;
     void* _object;
     bool _isReference = false;
 
-    void destroy()
-    {
-        if(!_isReference)
-        {
-            _type.destroy(_object);
-        }
-    }
+    void destroy();
 };
 
 }
