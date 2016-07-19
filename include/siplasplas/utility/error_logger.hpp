c3ac01ee0eb630becf8f09b036da27cc2840ac60
Manu343726 <Manu343726@gmail.com>
2016-07-19 18:52:40 +0200

    Error handling docs

--- include/siplasplas/utility/error_logger.hpp
+++ include/siplasplas/utility/error_logger.hpp
@@ -6,14 +6,80 @@
 #include <fmt/format.h>
 #include <siplasplas/utility/export.hpp>
 
+/**
+ * \defgroup error-handling
+ * \ingroup utility
+ * \brief Error handling tools.
+ *
+ * This submodule contains tools to handle errors in different
+ * ways and get detailed error reports.
+ *
+ * \todo cpp::Expect template for "fast" error handling.
+ */
+
 namespace cpp
 {
 
 namespace utility
 {
 
+/**
+ * \ingroup error-handling
+ * \brief Isolated logger for detailed error logging.
+ *
+ * This function returns a logger pointing to an isolated siplasplas
+ * errors log file. All exceptions and assertion failures are logged
+ * throught this logger.
+ *
+ * @return Reference to the spdlog logger.
+ */
 SIPLASPLAS_UTILITY_EXPORT spdlog::logger& errorLogger();
 
+/**
+ * \ingroup error-handling
+ * \brief Log an error with its backtrace. Also skip the latest \p framesToSkip stack frames from stack trace.
+ *
+ * This function logs a detailed error report with a stack trace ending at the caller function.
+ * Also supports skipping the last \p framesToSkip stack frames from the trace in case the
+ * trace is invoked from an API with multiple implementation layers.
+ * The generated report follows the format:
+ *
+ * ```
+ * #i "<binary file>", at <address> in <function>
+ * #i-1 "<binary file>", at <address> in <function>
+ * ...
+ * #1 "<binary file>", at <address> in <function>
+ * #0 "<binary file>", at <address> in <function>
+ *
+ * <error message>
+ * ```
+ *
+ * where the lower (of index 0) line in the backtrace is the latest (caller) stack frame.
+ * The error message is logged with critical level and the backtrace with debug level.
+ * The backtrace supports a max of 32 frames. If the backtrace is longer, uppermost frames
+ * will be ignored.
+ *
+ * \param framesToSkip Number of lower frames to skip from the backtrace.
+ * \param message Error message. May contain argument placeholders. See [`fmt::format()`](http://fmtlib.net/latest/api.html#_CPPv2N3fmt6formatE10CStringRef7ArgList).
+ * \param args Error message arguments.
+ *
+ * ``` cpp
+ * void justBecauseExceptionsAreSlow(int i)
+ * {
+ *    // Because we miss nodejs, let's callback everything!
+ *    auto callback = []
+ *    {
+ *        if(i != 42)
+ *        {
+ *            // Log an error skipping the callback frame:
+ *            cpp::logErrorSkippingFrames(1, "Error: {} is not a valid answer Deep Thought!", i);
+ *        }
+ *    };
+ *
+ *    callback();
+ * }
+ * ```
+ */
 template<typename Message, typename... Args>
 void logErrorSkippingFrames(std::size_t framesToSkip, const Message& message, Args&&... args)
 {
@@ -47,6 +113,19 @@ void logErrorSkippingFrames(std::size_t framesToSkip, const Message& message, Ar
     errorLogger().critical(fmt::format(message, std::forward<Args>(args)...));
 }
 
+/**
+ * \ingroup error-handling
+ * \brief Logs an error with its backtrace. See cpp::logErrorSkippingFrames().
+ *
+ * Effectively calls `logErrorSkippingFrames(1, message, args...)`.
+ *
+ * \param message Error message. May contain argument placeholders.
+ * \param args Error message arguments.
+ *
+ * ``` cpp
+ * logError("unexpected error! (errno={}", errno);
+ * ```
+ */
 template<typename Message, typename... Args>
 void logError(const Message& message, Args&&... args)
 {
--- include/siplasplas/utility/exception.hpp
+++ include/siplasplas/utility/exception.hpp
@@ -9,18 +9,79 @@
 namespace cpp
 {
 
+/**
+ * \ingroup error-handling
+ * \brief Logs an error message from an exception.
+ *
+ * This function logs an error report (See cpp::logError()) about a thrown
+ * exception. The error report is of the form:
+ *
+ * ```
+ * '<exception type>' exception thrown: <message>
+ * ```
+ *
+ * \tparam Ex Type of the exception.
+ * \param message Exception message.
+ * \param framesToSkip Number of frames to skip from the backtrace. Default is cero. See cpp::logErrorSkippingFrames().
+ *
+ * ``` cpp
+ * try
+ * {
+ *    throwingMan();
+ * }
+ * catch(const std::runtime_error& ex)
+ * {
+ *     logException<std::runtime_error>(ex.what());
+ * }
+ * ```
+ */
 template<typename Ex, typename Message>
 void logException(const Message& message, std::size_t framesToSkip = 0)
 {
     cpp::utility::logErrorSkippingFrames(framesToSkip + 1, "'{}' exception thrown: {}", ctti::type_id<Ex>().name(), message);
 }
 
+/**
+ * \ingroup error-handling
+ * \brief Logs an exception.
+ *
+ * This function logs a detailed error report from an exception object.
+ * See cpp::logError(). The error report is of the form:
+ * ```
+ * '<exception type>' exception thrown: <exception.what()>
+ * ```
+ *
+ * \param exception Exception object. The error message for the report is
+ * get from `exception.what()`.
+ * \param framesToSkip Number of frames to skip from the backtrace. Default is cero. See cpp::logErrorSkippingFrames().
+ *
+ * ``` cpp
+ * try
+ * {
+ *    throwingMan();
+ * }
+ * catch(const std::runtime_error& ex)
+ * {
+ *     logException(ex);
+ * }
+ * ```
+ */
 template<typename Ex>
 void logException(const Ex& exception, std::size_t framesToSkip = 0)
 {
     logException<Ex>(exception.what(), framesToSkip + 1);
 }
 
+/**
+ * \brief Declares a siplasplas exception type.
+ *
+ * This CRTP provides a convenient way to declare exception types
+ * that follow siplasplas conventions. Siplasplas exceptions are automatically
+ * logged to the error logger (See cpp::logException()) on construction.
+ *
+ * \tparam Ex Declared exception type. It's the derived class parameter of the CRTP.
+ * \tparam Base Exception base class. std::exception by default.
+ */
 template<typename Ex, typename Base = std::exception>
 class Exception : public Base
 {
@@ -39,6 +100,40 @@ public:
     }
 };
 
+/**
+ * \ingroup error-handling
+ * \brief Instances an exception with a custom error message.
+ *
+ * This function creates an exception object of the given type
+ * using a custom parameterized error message (See [`fmt::format()`](http://fmtlib.net/latest/api.html#_CPPv2N3fmt6formatE10CStringRef7ArgList)).
+ * Exceptions instanced throught this function are reported automatically
+ * (See cpp::logError() and cpp::logException()).
+ *
+ * \tparam Exception Exception type to instance.
+ * \param framesToSkip Number of lower frames to skip from the report backtrace. See cpp::logErrorSkippingFrames().
+ * \param message Exception error message. May have message argument placeholders. See [`fmt::format()`](http://fmtlib.net/latest/api.html#_CPPv2N3fmt6formatE10CStringRef7ArgList).
+ * \param args Error message arguments.
+ *
+ * \return A \p Exception instance with the specified error message.
+ *
+ * ``` cpp
+ *
+ * template<typename Function>
+ * void businessLogic(Function callback)
+ * {
+ *     callback();
+ * }
+ *
+ * int main()
+ * {
+ *     businessLogic([]
+ *     {
+ *         // throw an error skipping the callback frame from the report backtrace:
+ *         throw exceptionSkippingFrames<std::runtime_error>(1, "Look, a runtime error!");
+ *     });
+ * }
+ * ```
+ */
 template<typename Exception, typename... Args>
 Exception exceptionSkippingFrames(std::size_t framesToSkip, const std::string& message, Args&&... args)
 {
@@ -55,11 +150,47 @@ Exception exceptionSkippingFrames(std::size_t framesToSkip, const std::string& m
 }
 
 template<typename Exception, typename... Args>
+/**
+ * \ingroup error-handling
+ * \brief Instances an exception with a custom error message.
+ *
+ * This function creates an exception object of the given type
+ * using a custom parameterized error message (See [`fmt::format()`](http://fmtlib.net/latest/api.html#_CPPv2N3fmt6formatE10CStringRef7ArgList)).
+ * Exceptions instanced throught this function are reported automatically
+ * (See cpp::logError() and cpp::logException()).
+ *
+ * \tparam Exception Exception type to instance.
+ * \param message Exception error message. May have message argument placeholders. See [`fmt::format()`](http://fmtlib.net/latest/api.html#_CPPv2N3fmt6formatE10CStringRef7ArgList).
+ * \param args Error message arguments.
+ *
+ * \return A \p Exception instance with the specified error message.
+ *
+ * ``` cpp
+ *
+ * template<typename Function>
+ * void businessLogic(Function callback)
+ * {
+ *     callback();
+ * }
+ *
+ * int main()
+ * {
+ *     businessLogic([]
+ *     {
+ *         // throw an error skipping the callback frame from the report backtrace:
+ *         throw exceptionSkippingFrames<std::runtime_error>(1, "Look, a runtime error!");
+ *     });
+ * }
+ * ```
+ */
 Exception exception(const std::string& message, Args&&... args)
 {
     return exceptionSkippingFrames<Exception>(1, message, std::forward<Args>(args)...);
 }
 
+/**
+ * \brief Throws an exception. See cpp::exception().
+ */
 template<typename Exception, typename... Args>
 void Throw(const std::string& message, Args&&... args)
 {
