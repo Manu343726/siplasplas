6ea68e08cddb7dd998922792602aa3854d14e231
Manu343726 <Manu343726@gmail.com>
2016-07-20 17:01:26 +0200

    Utility.lexical_cast docs

--- include/siplasplas/utility/lexical_cast.hpp
+++ include/siplasplas/utility/lexical_cast.hpp
@@ -6,6 +6,20 @@
 namespace cpp
 {
 
+/**
+ * \ingroup utility
+ * \brief Returns an string representation of a value.
+ *
+ * This function invokes `operator<<(std::ostream&, const T&)` to get
+ * an string representation of a given value.
+ *
+ * It's a simplified version of the [Boost.Lexical_Cast library](http://www.boost.org/doc/libs/1_55_0/doc/html/boost_lexical_cast.html).
+ *
+ * \tparam T value type. Must have a valid `operator<<(std::ostream&, T)` overload.
+ * \param value Value which is translated to a string.
+ *
+ * @return A string representation of the given value.
+ */
 template<typename T>
 std::string lexical_cast(const T& value)
 {
@@ -16,6 +30,20 @@ std::string lexical_cast(const T& value)
     return os.str();
 }
 
+/**
+ * \ingroup utility
+ * \brief Returns an string representation of a value.
+ *
+ * This function invokes `operator>>(std::istream&, T&)` to assign a T value
+ * from an string representation of it.
+ *
+ * It's a simplified version of the [Boost.Lexical_Cast library](http://www.boost.org/doc/libs/1_55_0/doc/html/boost_lexical_cast.html).
+ *
+ * \tparam T Required output value type. Must have a valid `operator>>(std::istream&, T&)` overload.
+ * \param value String representation of the value.
+ *
+ * @return A value of type \p T equivalent to the \p value string representation argument.
+ */
 template<typename T>
 T lexical_cast(const std::string& value)
 {
