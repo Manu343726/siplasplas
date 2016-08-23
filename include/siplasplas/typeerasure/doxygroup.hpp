4caffd811b299abf68be7645b445debc3682bfe0
Manu343726 <Manu343726@gmail.com>
2016-08-23 19:06:44 +0200

    [#24] Typeerasure doxygroup fixes

--- include/siplasplas/typeerasure/doxygroup.hpp
+++ include/siplasplas/typeerasure/doxygroup.hpp
@@ -5,7 +5,7 @@
  * This module provides classes and methods to manipulate type-erased objects:
  *
  *  - `cpp::SimpleAny`: Type-erased value container with customizable storage
- *  - `cpp::typeerasure::Function`: Type-erased callable customizable storage
+ *  - `cpp::typeerasure::Function`: Type-erased callable with customizable storage
  *  - `cpp::Any`: Type-erased value container with a customizable object-like interface
  *  at runtime.
  *  - `cpp::typeerasure::invoke()`: Generic callable invocation with type-erased arguments
