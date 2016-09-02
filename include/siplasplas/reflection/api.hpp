59f018388c14c14c276c60204b23a1f57ef898bd
Manuel Sánchez <manuel@by.com.es>
2016-09-02 14:45:34 +0200

    cpp::static_reflection::Class docs

--- include/siplasplas/reflection/api.hpp
+++ include/siplasplas/reflection/api.hpp
@@ -1,6 +1,11 @@
 #ifndef SIPLASPLAS_REFLECTION_API_HPP
 #define SIPLASPLAS_REFLECTION_API_HPP
 
+/**
+ * \defgroup reflection
+ * \brief Access to static and dynamic reflection information
+ */
+
 #include "static/api.hpp"
 #include "dynamic/api.hpp"
 
