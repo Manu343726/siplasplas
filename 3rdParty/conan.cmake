51af4b201178399009836eaacb5628887cfc25f1
Manu343726 <Manu343726@gmail.com>
2016-12-29 21:20:29 +0100

    [#83] Fix conan.io config
    The conan.cmake script was importing the old
    3rdParty/conanfile.cmake conan output file, generated when
    conan was being invoked in the 3rdParty directory.
    
    See cbddd10 for the changes that originated this error.

--- 3rdParty/conan.cmake
+++ 3rdParty/conan.cmake
@@ -1,9 +1,9 @@
 execute_process(
-    COMMAND conan install --build=outdated --file "${CMAKE_SOURCE_DIR}/3rdParty/conanfile.txt"
+    COMMAND conan install --build=outdated
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
 )
 
-include(${CMAKE_SOURCE_DIR}/3rdParty/conanbuildinfo.cmake)
+include(${CMAKE_SOURCE_DIR}/conanbuildinfo.cmake)
 conan_basic_setup()
 conan_define_targets()
 include(${CONAN_CMAKE-UTILS_ROOT}/conan.cmake)
